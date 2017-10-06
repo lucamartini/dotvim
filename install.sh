5671034afdbd07670d1b9aa2084b4b122bb0f514
luca <luca.martini82@gmail.com>
2017-10-06 15:59:31 +0200

    fix .

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Set up the symlinks to vimrc and gvimrc
 unlink ~/.vimrc
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
 
 unlink ~/.gvimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
 
