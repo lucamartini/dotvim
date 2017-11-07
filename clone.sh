6f4ae12dcd779b71551c88e30adbf1785b011d1e
Luca Martini <luca.martini82@gmail.com>
2017-11-07 11:30:15 +0100

    youcompleteme and new script

--- clone.sh
+++ clone.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
-rm -rf ~/.vim
-git clone git://github.com/lucamartini/dotvim.git ~/.vim
+FOLDER=~/.vim
+URL=git://github.com/lucamartini/dotvim.git 
 
-cd ~/.vim
+if [ ! -d "$FOLDER" ] ; then
+    git clone "$URL" "$FOLDER"
+else
+    cd "$FOLDER"
+    git pull "$URL"
+fi
 
-sh install.sh
-
-cd pack
+cd ~/.vim/pack
 ./install.sh
+
+# youcompleteme install
+cd ~/.vim/pack/edit_style/start/YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer --js-completer --system-libclang
+
+# Set up the symlinks to vimrc and gvimrc
+unlink ~/.vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+
+unlink ~/.gvimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
 
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Set up the symlinks to vimrc and gvimrc
-unlink ~/.vimrc
-ln -s ~/.vim/vimrc ~/.vimrc
-
-unlink ~/.gvimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-
