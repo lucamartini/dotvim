3ad9eb9cc77cced5e646d6f0995aa297cea397e3
Luca Martini <luca.martini82@gmail.com>
2017-11-12 22:02:48 +0100

    https://github.com/weynhamz/vim-plugin-minibufexpl.git

--- pack/install.sh
+++ pack/install.sh
@@ -68,6 +68,7 @@ function package () {
   package https://github.com/Xuyuanp/nerdtree-git-plugin
   # package https://github.com/ryanoasis/vim-devicons
   package https://github.com/tiagofumo/vim-nerdtree-syntax-highlight.git
+  package https://github.com/weynhamz/vim-plugin-minibufexpl.git
   wait
 ) &
 
