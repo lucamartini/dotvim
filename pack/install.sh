c614d002b530ca5a9e76ff0f2adfb2d00f4c854b
Luca Martini <luca.martini82@gmail.com>
2017-11-16 16:15:24 +0100

    javascript hl

--- pack/install.sh
+++ pack/install.sh
@@ -52,6 +52,7 @@ function package () {
   package https://github.com/kshenoy/vim-signature
   package https://github.com/airblade/vim-gitgutter
   package https://github.com/flazz/vim-colorschemes
+  package https://github.com/pangloss/vim-javascript
   wait
 ) &
 
